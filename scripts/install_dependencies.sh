ba1c6bb5b00326cd8e873c0c246d5c33736d86b4
Ryan Nazareth <ryankarlos@gmail.com>
2022-06-12 19:03:26 +0100

    switch psyconfg2 to binary

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
+sudo yum update -y
+sudo yum install ruby -y
+sudo yum install wget -y
 sudo amazon-linux-extras install python3.8 -y
+sudo yum install postgresql postgresql-devel python-devel -y # adds pg_config required for pip install psycopg2
 cd /home/ec2-user/sample-app
-sudo pip3 install -r requirements.txt
+sudo python3.8 -m pip install -r requirements.txt
