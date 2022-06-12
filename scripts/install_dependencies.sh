9b154ea59454adee81a086e918cb1c6407998e7b
Ryan Nazareth <ryankarlos@gmail.com>
2022-06-12 19:29:06 +0100

    modify paths

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,7 @@ sudo yum install ruby -y
 sudo yum install wget -y
 sudo amazon-linux-extras install python3.8 -y
 sudo yum install postgresql postgresql-devel python-devel -y # adds pg_config required for pip install psycopg2
-cd /home/ec2-user/sample-app
+# if running this in appsepc yml 'Before install' stage, code deploy copies full repo into /opt/codedeploy-agent/deployment-root/
+# so this path to requirements.txt needs to be relative to appspec.yml.
+cd aws_vpc/eb-flask
 sudo python3.8 -m pip install -r requirements.txt
